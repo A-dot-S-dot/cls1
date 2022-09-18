bb0e7e49611eef64b12248dc74f63bf80a1a3863
A-dot-S-dot <alexey.schwarzmann@tu-dortmund.de>
2022-09-18 07:03:06 +0200

    Changed default plot elements number

--- defaults.py
+++ defaults.py
@@ -2,7 +2,7 @@
 COURANT_FACTOR = (
     10  # factor for calculation number of time steps depending on number of simplices
 )
-ELEMENTS_NUMBER_PLOT = 100
+ELEMENTS_NUMBER_PLOT = 200
 ELEMENTS_NUMBER_EOC = 8
 
 # finite element space
