ae2300e06d3ad0f6d7b2ee86d8e89210a506dd6b
A-dot-S-dot <alexey.schwarzmann@tu-dortmund.de>
2022-09-22 14:40:48 +0200

    Added lumped mass.

---
+++ system/vector/lumped_mass.py
@@ -0,0 +1,22 @@
+import numpy as np
+from fem import FiniteElementSpace
+from system.matrix.mass import MassMatrix
+
+from .system_vector import SystemVector
+
+
+class LumpedMassVector(SystemVector):
+    """Lumped mass system vector. It's entries are raw sums of mass matrix or
+    Integral(bi), where bi is finite element basis.
+
+    """
+
+    _mass: MassMatrix
+
+    def __init__(self, element_space: FiniteElementSpace):
+        SystemVector.__init__(self, element_space)
+        self._mass = MassMatrix(element_space)
+        self.assemble()
+
+    def assemble(self):
+        self[:] = np.sum(self._mass.values.toarray(), axis=1)
