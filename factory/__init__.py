2037c6e3e7af86c27642d2466130f94c4c9a7206
A-dot-S-dot <alexey.schwarzmann@tu-dortmund.de>
2022-10-01 09:45:47 -0500

    Deleted mesh factory.

--- factory/__init__.py
+++ factory/__init__.py
@@ -1,7 +1,6 @@
 from .artificial_diffusion_factory import *
 from .benchmark_factory import *
 from .flux_factory import *
-from .mesh_factory import *
 from .ode_solver_factory import *
 from .pde_solver_factory import *
 from .time_stepping_factory import *
