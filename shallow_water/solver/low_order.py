2a50aedf3b9e67b0895f83882b61ea9959a904cf
A-dot-S-dot <alexey.schwarzmann@tu-dortmund.de>
2023-02-17 20:15:07 +0100

    Refactored low_order: added attributes

--- shallow_water/solver/low_order.py
+++ shallow_water/solver/low_order.py
@@ -20,18 +20,34 @@ class LowOrderFlux(lib.NumericalFlux):
     """
 
     input_dimension = 2
-    bathymetry_step: np.ndarray
-    height_positivity_fix: np.ndarray
+    _bathymetry_step: np.ndarray
+    _value_left: np.ndarray
+    _value_right: np.ndarray
+    _height_HLL: np.ndarray
+    _discharge_HLL: np.ndarray
+    _height_positivity_fix: np.ndarray
+    _modified_height_left: np.ndarray
+    _modified_height_right: np.ndarray
+    _modified_discharge_left: np.ndarray
+    _modified_discharge_right: np.ndarray
 
     _riemann_solver: shallow_water.RiemannSolver
 
     @property
-    def g(self) -> float:
+    def _gravitational_acceleration(self) -> float:
         return self._riemann_solver.gravitational_acceleration
 
     @property
-    def wave_speed(self) -> np.ndarray:
+    def _wave_speed(self) -> np.ndarray:
         return self._riemann_solver.wave_speed_right
+
+    @property
+    def _flux_left(self) -> np.ndarray:
+        return self._riemann_solver.flux_left
+
+    @property
+    def _flux_right(self) -> np.ndarray:
+        return self._riemann_solver.flux_right
 
     def __init__(self, gravitational_acceleration: float, bathymetry=None):
         self._riemann_solver = shallow_water.RiemannSolver(
@@ -42,60 +58,72 @@ class LowOrderFlux(lib.NumericalFlux):
 
     def _build_topography_step(self, bathymetry: Optional[np.ndarray]):
         if bathymetry is None or shallow_water.is_constant(bathymetry):
-            self.bathymetry_step = np.array([0])
+            self._bathymetry_step = np.array([0])
         else:
             bathymetry = np.array([bathymetry[0], *bathymetry, bathymetry[-1]])
-            self.bathymetry_step = np.diff(bathymetry)
+            self._bathymetry_step = np.diff(bathymetry)
 
     def __call__(
         self, value_left: np.ndarray, value_right: np.ndarray
     ) -> Tuple[np.ndarray, np.ndarray]:
         bar_state, _ = self._riemann_solver.solve(value_left, value_right)
+        self._value_left, self._value_right = value_left, value_right
 
-        h_HLL, q_HLL = shallow_water.get_height_and_discharge(bar_state)
-
-        hl, hr = self._modify_height(h_HLL)
-        ql, qr = self._modify_discharge(
-            q_HLL,
-            np.average(shallow_water.get_velocities(value_left, value_right), axis=0),
+        self._height_HLL, self._discharge_HLL = shallow_water.get_height_and_discharge(
+            bar_state
         )
 
+        self._build_modified_heights()
+        self._modify_discharge()
+
+        left_state = np.array(
+            [self._modified_height_left, self._modified_discharge_left]
+        ).T
+        right_state = np.array(
+            [self._modified_height_right, self._modified_discharge_right]
+        ).T
+
+        return (
+            self._wave_speed[:, None] * (left_state + -value_left) + -self._flux_left,
+            self._wave_speed[:, None] * (right_state + -value_right) + self._flux_right,
+        )
+
+    def _build_modified_heights(self):
+        self._height_positivity_fix = get_height_positivity_fix(
+            self._height_HLL, self._bathymetry_step
+        )
+        self._modified_height_left = self._height_HLL + self._height_positivity_fix
+        self._modified_height_right = self._height_HLL - self._height_positivity_fix
+
+    def _modify_discharge(self):
         source_term = self.get_source_term(
-            np.average(shallow_water.get_heights(value_left, value_right), axis=0)
+            np.average(
+                shallow_water.get_heights(self._value_left, self._value_right), axis=0
+            )
         )
-
-        left_state = np.array([hl, ql + source_term]).T
-        right_state = np.array([hr, qr + source_term]).T
-
-        flux_left = self._riemann_solver.flux_left
-        flux_right = self._riemann_solver.flux_right
-
-        return (
-            self.wave_speed[:, None] * (left_state + -value_left) + -flux_left,
-            self.wave_speed[:, None] * (right_state + -value_right) + flux_right,
+        velocity_average = np.average(
+            shallow_water.get_velocities(self._value_left, self._value_right), axis=0
         )
-
-    def _modify_height(self, h_HLL: np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
-        self.height_positivity_fix = get_height_positivity_fix(
-            h_HLL, self.bathymetry_step
+        self._modified_discharge_left = (
+            self._discharge_HLL
+            + velocity_average * self._height_positivity_fix
+            + source_term
         )
-
-        return h_HLL + self.height_positivity_fix, h_HLL - self.height_positivity_fix
-
-    def _modify_discharge(
-        self, q_HLL: np.ndarray, velocity_average: np.ndarray
-    ) -> Tuple[np.ndarray, np.ndarray]:
-        return (
-            q_HLL + velocity_average * self.height_positivity_fix,
-            q_HLL + -velocity_average * self.height_positivity_fix,
+        self._modified_discharge_right = (
+            self._discharge_HLL
+            + -velocity_average * self._height_positivity_fix
+            + source_term
         )
 
     def get_source_term(self, height_average: np.ndarray) -> np.ndarray:
-        if self.bathymetry_step is None:
+        if self._bathymetry_step is None:
             return np.array([0.0])
         else:
             return -(
-                self.g * height_average * self.height_positivity_fix / self.wave_speed
+                self._gravitational_acceleration
+                * height_average
+                * self._height_positivity_fix
+                / self._wave_speed
             )
 
 
